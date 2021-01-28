876679e7f99dc40c4fe0bd8925e7aafe75f1f696
sanjar1311 <sanjarabduxamidov1311@gmail.com>
2021-01-27 23:29:26 -0600

    add js and complate p-2

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,22 @@ var createElement = function (tagName, className, text) {
 
   return element;
 };
+
+var colors = ['1','2','3','4','5','6','7','8','9','0','a','b','c','d','e','f','a'];
+
+var elBtn = $_('.btn');
+
+function getRandomNumber(array) {
+  return Math.floor(Math.random() * array.length);
+}
+
+
+function changeBg() {
+  var hex = '#';
+  for(var i = 0; i < 6; i++) {
+    hex +=`${colors[getRandomNumber(colors)]}`
+    document.body.style.backgroundColor = `${hex}`;
+  }
+}
+
+elBtn.addEventListener('click', changeBg);
